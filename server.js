d066901788330d7391284f3baffe194538271f9d
rabkool <905956347@qq.com>
2020-09-28 23:55:54 +0900

    Buffer decoding widget

--- server.js
+++ server.js
@@ -18,6 +18,26 @@ let userRouter = require('./router/userRouter')
 
 app.use('/user', userRouter)
 
+app.post('/decodebf', (req, res) => {
+    let { result } = req.body
+    if (!result) {
+        return res.send({ err: 999999999, msg: '入力してくださいよ。空はダメだよ' })
+    }
+    if (typeof result == 'string') {
+        try {
+            buf = Buffer.from(JSON.parse(result))
+            if (Buffer.isBuffer(buf)) {
+                rabkool = buf.toString()
+            } else {
+                return res.send({ err: 999999999, msg: 'Bufferを入力してくださいよ' })
+            }
+        } catch (e) {
+            return res.send({ err: 999999999, msg: 'Bufferを入力してください' })
+        }
+    }
+    return res.send('结果:' + rabkool)
+})
+
 app.listen(80, () => {
     console.log('server start')
 })
