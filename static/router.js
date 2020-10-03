5be60b2ad310b9fa5369f4cea43125c3f7bcef2a
rabkool <905956347@qq.com>
2020-10-03 22:29:10 +0900

    views

--- static/router.js
+++ static/router.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+
 router.get('/index', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+
+    let data = [
+        {
+            title: '彼女募集中', content: '彼女募集中'
+        }
+    ]
+    res.render('index', {data: data})
 })
 
 router.get('/login', (req, res) => {
